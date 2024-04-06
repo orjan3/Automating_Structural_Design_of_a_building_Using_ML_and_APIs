aaac4595c25295096d5db150028c468588f93a69
jorgeh <69015632+orjan3@users.noreply.github.com>
2024-04-06 17:04:26 -0500

    .

--- JAAD.DataBase.py
+++ JAAD.DataBase.py
@@ -1,34 +1,39 @@
-import pandas as pd, numpy as np
+import pandas as pd, numpy as np, Calcs as calcs
 
-# Crear los DataFrames con un bucle
-# C1 = fc, C2 = fy, C3 = Mu, C4 = Base, C5 = Popt, C6 = Ropt, C7 = dopt, C8 = Asopt
-dfs = []
-q=85; t=0.10; Φ=0.9; it=800
-for i in range(1, 5):
-    data = {'C1': [21]*it, 'C2': [414]*it, 'C3': [100]*it, 'C4': [200]*it}
-    #data[f'C{i}'] = list(range(it*(i-1)+1, it*i+1))
-    data[f'C{i}'] = list(range(1, it+1))
-    print(len(data[f'C{i}']))
-    dfs.append(pd.DataFrame(data))
+db = []; q=85; t=0.10; Φ=0.9; it=800
+ls = np.linspace
+data_list = [
+    {'fc': ls(21,56,it), 'fy': ls(420,420,it), 'Mu': ls(100,100,it),'b': ls(300,300,it)},
+    {'fc': ls(28,28,it), 'fy': ls(300,600,it), 'Mu': ls(100,100,it),'b': ls(300,300,it)},
+    {'fc': ls(28,28,it), 'fy': ls(420,420,it), 'Mu': ls(50,200,it),'b': ls(300,300,it)},
+    {'fc': ls(28,28,it), 'fy': ls(420,420,it), 'Mu': ls(100,100,it),'b': ls(200,800,it)}
+] 
 
-# Concatenar todos los DataFrames
-df = pd.concat(dfs, ignore_index=True)
+# Itera sobre los diccionarios y crea DataFrames
+for data_dict in data_list:
+    db.append(pd.DataFrame(data_dict))
 
-# Calcular la columna 5 (Popt)
+# Concatena los DataFrames en uno solo
+db = pd.concat(db, ignore_index=True)
 
-df['C5'] = 1/(q/(1+t)+(df['C2']/(0.85*df['C1'])))
+# Calcular la columna 5 (ß)
+db['ß']=calcs.Beta(db['fc'])
 
-# Calcular la columna 6 (Ropt)
-df['C6'] = 1/(df['C5']*df['C2']*(1-(df['C5']*df['C2']/(1.7*df['C1']))))**0.5
+# Calcular la columna 6 (Popt)
+db['Popt'] = 1/(q/(1+t)+(db['fy']/(0.85*db['fc'])))
 
-# Calcular la columna 7 (dopt)
-df['C7'] = (df['C6']*((df['C3']/Φ)/df['C4'])**0.5)*10**2
+# Calcular la columna 7 (Ropt)
+db['Ropt'] = 1/(db['Popt']*db['fy']*(1-(db['Popt']*db['fy']/(1.7*db['fc']))))**0.5
 
-# Calcular la columna 8 (Asopt)
-df['C8'] = df['C5']*df['C7']*df['C4']/10
-c8 = df['C5']*df['C7']*df['C4']/10
+# Calcular la columna 8 (dopt)
+db['dopt'] = (db['Ropt']*((db['Mu']/Φ)/db['b'])**0.5)*10**2
+
+# Calcular la columna 9 (Asopt)
+db['Asopt'] = db['Popt']*db['dopt']*db['b']/10
+
 # Imprimir el DataFrame resultante
-print(df)
+print(db)
 
 # Exportar a Excel
-df.to_excel('datos.xlsx', index=False)
+ColumnNames = ['fc', 'fy', 'Mu', 'b','ß', 'Popt', 'Ropt', 'dopt', 'Asopt']
+db.to_excel('JAAD.db.xlsx', index=False, columns=ColumnNames)
