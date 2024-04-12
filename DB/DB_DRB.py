9a26e80f7e73d8101e04d2258c653cf7da3c7447
jorgeh <69015632+orjan3@users.noreply.github.com>
2024-04-12 17:16:11 -0500

    .

--- DB/DB_DRB.py
+++ DB/DB_DRB.py
@@ -1,14 +1,14 @@
-import pandas as pd, numpy as np, Calcs as calcs, math
+import pandas as pd, numpy as np, Calcs as calcs
 fc = 20; fy = 400; b = 250; Mu = 150
 
-db = []; q=85; t=0.10; Φ=0.9; it=800; Q= 1+t; eu=0.003; et=0.021
+db = []; q=85; t=0.10; Φ=0.9; it=800; Q= 1+t; eu=0.003; et=0.005
 ls = np.linspace
 data_list = [
     {'fc': ls(21,56,it), 'fy': ls(fy,fy,it), 'Mu': ls(Mu,Mu,it),'b': ls(b,b,it)},
     {'fc': ls(fc,fc,it), 'fy': ls(300,600,it), 'Mu': ls(Mu,Mu,it),'b': ls(b,b,it)},
     {'fc': ls(fc,fc,it), 'fy': ls(fy,fy,it), 'Mu': ls(50,300,it),'b': ls(b,b,it)},
     {'fc': ls(fc,fc,it), 'fy': ls(fy,fy,it), 'Mu': ls(Mu,Mu,it),'b': ls(200,800,it)}
-] 
+]
 
 # Itera sobre los diccionarios y crea DataFrames
 for data_dict in data_list:
@@ -16,9 +16,11 @@ for data_dict in data_list:
 
 db = pd.concat(db, ignore_index=True)                                                       # Concatena los DataFrames en uno solo
 
-db['ß']=calcs.Beta(db['fc'])                                                                # Calcular la columna 5 (ß)                              # Calcular la columna 6 (Popt)
+db['ß']=calcs.Beta(db['fc'])       
 
-db['rho_u'] = 0.85*db['ß'] * (db['fc']/db['fy']) * (eu/(eu+et))
+db['rho_min'] = calcs.CuantiaMinima(db['fy'], db['fc'])         
+
+db['rho_u'] = calcs.CuantiaMaxima(db['ß'], db['fc'], db['fy'])
 
 db['rho_opt_p'] = (db['rho_u']*q*(db['rho_u']* db['fy']/(0.425*db['fc'])-(3+t))+(1-t)*Q)/(2*q*(1-t))
 
@@ -30,6 +32,4 @@ db['Asopt'] = (db['rho_u'] + db['rho_opt_p']) *db['b']*db['dopt'] /10
 
 db['Asopt_p'] = db['rho_opt_p'] *db['b']*db['dopt'] /10
 
-
-db.to_excel('db.xlsx', index=False)                    
-
+db.to_excel('db.xlsx', index=False)
