a6639a182ce088500ab6d3f8ded14942662a1978
jorge <elheazar3@gmail.com>
2024-04-20 09:24:09 -0500

    .

--- ScriptTools/ImageToText.py
+++ ScriptTools/ImageToText.py
@@ -1,46 +1,95 @@
-#                  IMPORTANT!!
-# Go to: https://github.com/UB-Mannheim/tesseract/wiki
-# Install the executable and come back to the script
+import os
+import sys
+import clr
 
-from PIL import Image
-from pytesseract import pytesseract
-import enum
+clr.AddReference("System.Runtime.InteropServices")
+from System.Runtime.InteropServices import Marshal
+
+#set the following path to the installed ETABS program directory
+clr.AddReference(R'C:\Program Files\Computers and Structures\ETABS 20\ETABSv1.dll')
+from ETABSv1 import *
+
+#set the following flag to True to execute on a remote computer
+Remote = False
+
+#if the above flag is True, set the following variable to the hostname of the remote computer
+#remember that the remote computer must have ETABS installed and be running the CSiAPIService.exe
+RemoteComputer = "SpareComputer-DT"
+
+#set the following flag to True to attach to an existing instance of the program
+#otherwise a new instance of the program will be started
+AttachToInstance = True
+
+#set the following flag to True to manually specify the path to ETABS.exe
+#this allows for a connection to a version of ETABS other than the latest installation
+#otherwise the latest installed version of ETABS will be launched
+SpecifyPath = True
+
+#if the above flag is set to True, specify the path to ETABS below
+ProgramPath = "C:\Program Files\Computers and Structures\ETABS 20\ETABS.exe"
+
+#full path to the model
+#set it to the desired path of your model
+APIPath = 'C:\CSi_ETABS_API_Example'
+if not os.path.exists(APIPath):
+    try:
+        os.makedirs(APIPath)
+    except OSError:
+        pass
+ModelPath = APIPath + os.sep + 'API_1-001.edb'
+
+#create API helper object
+helper = cHelper(Helper())
+
+if AttachToInstance:
+    #attach to a running instance of ETABS
+    try:
+        #get the active ETABS object        
+        if Remote:
+            myETABSObject = cOAPI(helper.GetObjectHost(RemoteComputer, "CSI.ETABS.API.ETABSObject"))
+        else:
+            myETABSObject = cOAPI(helper.GetObject("CSI.ETABS.API.ETABSObject"))
+    except:
+        print("No running instance of the program found or failed to attach.")
+        sys.exit(-1)
+
+else:
+    if SpecifyPath:
+        try:
+            #'create an instance of the ETABS object from the specified path
+            if Remote:
+                myETABSObject = cOAPI(helper.CreateObjectHost(RemoteComputer, ProgramPath))
+            else:
+                myETABSObject = cOAPI(helper.CreateObject(ProgramPath))
+        except :
+            print("Cannot start a new instance of the program from " + ProgramPath)
+            sys.exit(-1)
+    else:
+
+        try: 
+            #create an instance of the ETABS object from the latest installed ETABS
+            if Remote:
+                myETABSObject = cOAPI(helper.CreateObjectProgIDHost(RemoteComputer, "CSI.ETABS.API.ETABSObject"))
+            else:
+                myETABSObject = cOAPI(helper.CreateObjectProgID("CSI.ETABS.API.ETABSObject"))
+
+        except:
+            print("Cannot start a new instance of the program.")
+            sys.exit(-1)
 
 
-class OS(enum.Enum):
-    Mac = 0
-    Windows = 1
 
+#create SapModel object
+SapModel = cSapModel(myETABSObject.SapModel)
 
-class Language(enum.Enum):
-    ENG = "eng"
-    RUS = "rus"
-    ITA = "ita"
+#initialize model
+SapModel.InitializeNewModel()
 
+#create new blank model
+File = cFile(SapModel.File)
+ret = File.NewBlank()
 
-class ImageReader:
-    def __init__(self, os: OS):
-        if os == OS.Mac:
-            print("Running on: Mac\n")
-        if os == OS.Windows:
-            # Replace the address for the one you saved it when you installed it
-            # I installed it on that address. Change it.
-            # For that, your need to install the PyTesseract first
-            Windows_path = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-            pytesseract.tesseract_cmd = Windows_path
-            print("Running on: Windows\n")
+#define material property
 
-    def extract_text(self, image: str, lang: str) -> str:
-        img = Image.open(image)
-        extracted_text = pytesseract.image_to_string(img, lang=lang)
-        return extracted_text
+ret = SapModel.PropMaterial.SetMaterial("Concrete", eMatType.Concrete)
 
-
-if __name__ == "__main__":
-    ir = ImageReader(OS.Windows)
-    # This is the address where you have your image to be converted.
-    text = ir.extract_text(
-        r"C:\Users\JORGE\OneDrive\Imágenes\Screenshots\Captura de pantalla 2024-04-16 201023.png",
-        Language.ENG.value,
-    )
-    print(text)
